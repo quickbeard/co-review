ab129380ba11113976578b9f2afe9985961d64db
minhn4 <nguyenm.cuny@gmail.com>
2026-04-03 17:02:35 +0700

    delete migrations

--- dashboard/prisma/migrations/20260403100118_init/migration.sql
+++ dashboard/prisma/migrations/20260403100118_init/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "GitProviderType" AS ENUM ('github', 'gitlab', 'bitbucket', 'azure_devops', 'gitea', 'gerrit');
 
@@ -13,15 +6,6 @@ CREATE TYPE "LLMServerType" AS ENUM ('litellm', 'openrouter', 'ollama', 'openai'
 
 -- CreateEnum
 CREATE TYPE "ReviewStatus" AS ENUM ('pending', 'in_progress', 'completed', 'failed');
-
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "Post";
-
--- DropTable
-DROP TABLE "User";
 
 -- CreateTable
 CREATE TABLE "Organization" (
